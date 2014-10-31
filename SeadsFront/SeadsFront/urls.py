245d0947ea86edcef4c50906e424d8e537ef56c3
Jason Heron <JasonAHeron@Gmail.com>
2014-10-31 16:23:05 -0700

    cleaning things up a bit

--- SeadsFront/SeadsFront/urls.py
+++ SeadsFront/SeadsFront/urls.py
@@ -4,19 +4,15 @@ from seadssite import views as v
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'SeadsFront.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', v.IndexView.as_view()),
     url(r'^dashboard/',v.DashboardView),
     url(r'^visualization/', v.VisualizationView),
-    #Rita Commented out just in case we need again url(r'^devices/', v.DevicesView),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', 'django.contrib.auth.views.logout',
     	{'next_page': '/'}),
     url(r'^register/$', v.register),
-)
+                        )
 
 urlpatterns += staticfiles_urlpatterns()
--- SeadsFront/seadssite/views.py
+++ SeadsFront/seadssite/views.py
@@ -68,80 +68,33 @@ def register(request):
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    # Render the template depending on the context.
     return render_to_response(
             'register.html',
             {'user_form': user_form, 'profile_form': profile_form, 'registered': registered},
             context)
 
 def DashboardView(request):
-    device_set = []
     device_id = 0
-    #get current user instance of Users model
-    current_user = request.user
-    #get devices current user owns via Map model
+    current_user = request.user    
     user_devices = Map.objects.filter(user_id=current_user.id)
-    #if user hits "register"
+
     if(request.POST.get('register')):
-        #get id they put in
         new_device_id = request.POST.get('device_id')
         new_device_name = request.POST.get('device_name')
-        #these need to have API calls to ask the DB about device status
-        #new_device_connection = False
-        #new_device_power = False
-        #create a new device and save it to the DB
         try:
             D = Devices(device_id=new_device_id, name=new_device_name)
-            D.save() 
-        #create a new Map from the current user to the new device      
+            D.save()     
             Map(user = current_user, device = D).save()
         except ValueError:
             print "Invalid Device ID"
         except TypeError:
             print "Invalid Device ID"
-    #if user hits "delete" button    
+
     elif(request.POST.get('delete')):
-        #get id of value to delete
         device_id = request.POST.get('delete')
-        #delete it
         Devices.objects.filter(device_id = device_id).delete()
 
-
     return render(request, 'dashboard.html', {'devices': user_devices, 'device_id': device_id})
-
-# NOTE: Rita Commented out in case we need it for something else.
-# def DevicesView(request):
-#     device_set = []
-#     device_id = 0
-#     current_user = request.user
-#     user_devices = Map.objects.filter(user_id=current_user.id)
-#     for d in user_devices:
-#         device_set.append(d.get_device())
-#     if(request.POST.get('register')):
-#         #get id they put in
-#         new_device_id = request.POST.get('device_id')
-#         new_device_name = request.POST.get('device_name')
-#         #these need to have API calls to ask the DB about device status
-#         #new_device_connection = False
-#         #new_device_power = False
-#         #create a new device and save it to the DB
-#         #create a new Map from the current user to the new device      
-#         try:
-#             D = Devices(device_id=new_device_id, device_name=new_device_name)
-#             D.save() 
-#             Map(user = current_user, device = D).save()
-#         except ValueError:
-#             print 'Invalid Device ID!'
-#         except TypeError:
-#             print 'Invalid Device ID!'
-#        #if user hits "delete" button    
-#     elif(request.POST.get('delete')):
-#         #get id of value to delete
-#         device_id = request.POST.get('delete')
-#         #delete it
-#         Devices.objects.filter(device_id = device_id).delete()
-
-#     return render(request, 'devices.html', {'devices': device_set, 'device_id': device_id})
 
 '''
 workflow:
