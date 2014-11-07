4a6c950c820bd31bdece96ea5cea931d90c04f7d
Brittany Arthur <brittany_arthur@hotmail.com>
2014-11-07 12:29:25 -0800

    broken, but working on edit functionality of dashboard

--- SeadsFront/SeadsFront/static/x-editable/js/x-main.js
+++ SeadsFront/SeadsFront/static/x-editable/js/x-main.js
@@ -2,6 +2,19 @@ $(document).ready(function() {
     //toggle `popup` / `inline` mode
     $.fn.editable.defaults.mode = 'inline';     
     //make username editable
-    $('#dev_name').editable();
 
+    $('#dev_name').editable({
+					type: 'text',
+   					 url: '/',  
+   					 ajaxOptions: {
+   					     type: 'post'
+  					  }  
+				  });
+
+    $.ajax({
+    url: '/',
+    response: function(settings) {
+        console.log(settings);
+       }
+    }); 
 });
