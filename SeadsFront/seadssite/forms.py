89c50a60d5b9aa099d4e6446ead85eb981624932
Brittany Arthur <brittany_arthur@hotmail.com>
2014-11-23 18:23:47 -0800

    added email confirmation. beggining work on registration form authent.

--- SeadsFront/seadssite/forms.py
+++ SeadsFront/seadssite/forms.py
@@ -1,13 +1,16 @@
-from seadssite.models import UserProfile
+from seadssite.models import UserProfile 
 from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
-    class Meta:
-        model = User
-        fields = ('username', 'first_name','last_name', 'email', 'password')
+#we should use email validation: http://stackoverflow.com/questions/3217682/checking-validity-of-email-in-django-python
+
+
+class Meta:
+    model = User
+    fields = ('username', 'first_name','last_name', 'email', 'password')
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
