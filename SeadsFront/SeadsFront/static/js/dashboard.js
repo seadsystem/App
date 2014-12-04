71d343e13f62ccb90116438244901055386d762e
Jason Heron <JasonAHeron@Gmail.com>
2014-12-03 20:53:55 -0800

    working on dashboard cleanup and api connection

---
+++ SeadsFront/SeadsFront/static/js/dashboard.js
@@ -0,0 +1,48 @@
+function validation(){
+	$("#deviceIDNumError").hide();
+	$("#deviceIDEmpty").hide();
+	$("#deviceNameEmpty").hide();
+	var deviceID = $("#device_id").val();
+	if (isNaN(deviceID)){
+		//alert("not a number");
+		$("#deviceIDNumError").show();
+		return false;
+	}
+	if(!$("#device_id").val()){
+		$("#deviceIDEmpty").show();
+		return false;
+	}
+	if(!$("#device_name").val()){
+		$("#deviceNameEmpty").show();
+		return false;	
+	}
+	else{
+		$("#deviceRegisterSubmit").click();
+	}
+}
+
+$("#device_id, #device_name").keypress(function(e){ 
+	if(e.which == 13) {
+		e.preventDefault();
+		validation();
+	}
+});
+
+function get_api_data(device_id) {
+  $.get("", {device_id:device_id}, function(data) {
+    console.log(data);
+    });
+}
+
+$(document).ready(function(){
+	$(".delete").click(function(event){
+	  event.stopPropagation();
+	});
+	$(".deviceBox").click(function(event){
+	  window.location.href = "/devices/";
+	});
+});
+
+$( ".col-md-3 .registerDevice" ).mouseover(function() {
+  console.log("mouseover event detected.");
+});  
--- SeadsFront/seadssite/views.py
+++ SeadsFront/seadssite/views.py
@@ -118,6 +118,9 @@ def DashboardView(request):
         if alert is not None:
             alerts.append(alert)
 
+    elif request.is_ajax():
+        
+
     return render(request, 'dashboard.html', {'maps': user_devices_map, 'alerts':alerts})
 
 
